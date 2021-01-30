7f37cdde71d2e2f98640ef8e14319e3485be12bf
towermadman <towermadman@gmail.com>
2021-01-30 02:06:16 -0800

    Exercise 1.2 complete.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,11 +6,23 @@ const Header = (props) => {
 		<h1>{props.course}</h1>
 	)
 }
-const Content = (props) => {
+
+const Part = (props) => {
 	return (
 		<p>{props.part} {props.exercises}</p>
 	)
 }
+
+const Content = (props) => {
+	return (
+		<div>
+			<Part part={props.part1} exercises={props.exercises1} />
+			<Part part={props.part2} exercises={props.exercises2} />
+			<Part part={props.part3} exercises={props.exercises3} />
+		</div>
+	)
+}
+
 const Total = (props) => {
 	return (
 		<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -29,9 +41,10 @@ const App = () => {
 	return (
 		<div>
 			<Header course={course} />
-			<Content part={part1} exercises={exercises1} />
-			<Content part={part2} exercises={exercises2} />
-			<Content part={part3} exercises={exercises3} />
+			<Content
+				exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+				part1={part1} part2={part2} part3={part3}
+			/>
 			<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
 		</div>
 	)
