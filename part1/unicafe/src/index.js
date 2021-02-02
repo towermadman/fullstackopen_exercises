7eaac55638ac4cb0de794550446f2eeb8094e834
towermadman <towermadman@gmail.com>
2021-02-02 01:44:42 -0800

    Exercise 1.7 complete.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,16 +11,38 @@ const Button = ({ onClick, text }) => {
 	)
 }
 
-const Feedback = ({ text, feedback }) => <p>{text} {feedback}</p>
+const Statistics = ({ text, value }) => <p>{text} {value}</p>
 
 const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
-	const goodButton = () => setGood(good + 1)
-	const neutralButton = () => setNeutral(neutral + 1)
-	const badButton = () => setBad(bad + 1)
+	const [all, setAll] = useState(0)
+	const [average, setAverage] = useState(0)
+	const [positive, setPositive] = useState(0)
+
+	const goodButton = () => {
+		setGood(good + 1)
+		setAll(all + 1)
+
+		setAverage((good - bad + 1) / (all + 1))
+		setPositive((good + 1) / (all + 1))
+	}
+	const neutralButton = () => {
+		setNeutral(neutral + 1)
+		setAll(all + 1)
+
+		setAverage((good - bad) / (all + 1))
+		setPositive(good / (all + 1))
+	}
+	const badButton = () => {
+		setBad(bad + 1)
+		setAll(all + 1)
+
+		setAverage((good - bad - 1) / (all + 1))
+		setPositive(good / (all + 1))
+	}
 
 	return (
 	<div>
@@ -33,9 +55,13 @@ const App = () => {
 
 		<Header header="statistics" />
 		<div>
-			<Feedback text="good" feedback={good} />
-			<Feedback text="neutral" feedback={neutral} />
-			<Feedback text="bad" feedback={bad} />
+			<Statistics text="good" value={good} />
+			<Statistics text="neutral" value={neutral} />
+			<Statistics text="bad" value={bad} />
+
+			<Statistics text="all" value={all} />
+			<Statistics text="average" value={average} />
+			<Statistics text="positive" value={positive * 100 + "%"} />
 		</div>
   	</div>
 	)
